dd6046385f3a44d6ca53610e07c8b18376efa8d3
Andresss <53404779+Andress-s@users.noreply.github.com>
2023-06-04 17:52:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,7 +7,9 @@
 
 std::vector<char> getPerm(const Tree& tree, int n) {
   std::vector<std::vector<char>> perms = tree.getPerms();
-  if (!(perms.size() < n)) {
+  if (perms.size() >= n) {
     return perms[n - 1];
+  } else {
+    return {};
   }
 }
